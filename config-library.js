5c48dc988ecf2d587d5e176447855c7d4e803471
lujo5 <lukamatosevic5@gmail.com>
2019-01-26 22:15:16 +0100

    Fixed config-library from previous version.

--- config-library.js
+++ config-library.js
@@ -1,9 +1,8 @@
-export const nameLibrary = "ng5restful";
+export const nameLibrary = "ngx-restful";
 export const PATH_SRC = "lib/";
 export const PATH_DIST = "dist/";
 export const EXTERNAL_LIBS = [
     '@angular/core',
     '@angular/common',
     'rxjs',
-    'rxjs-compat'
 ];
