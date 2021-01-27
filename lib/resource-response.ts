009aae3e04ea83f7913790ff9514cb773c29b628
lmatosevic <lukamatosevic5@gmail.com>
2021-01-27 21:20:18 +0100

    Separated resource response and list.

--- lib/resource-response.ts
+++ lib/resource-response.ts
@@ -1,8 +1,15 @@
-export class ResourceResponse<T> {
+export class ResourceResponseBase {
     success: boolean;
     message: string;
     total_count: number;
     total_pages: number;
     result_count: number;
-    data: Array<T> | T = [];
+}
+
+export class ResourceResponse<T> extends ResourceResponseBase {
+    data: T = null;
+}
+
+export class ResourceResponseList<T> extends ResourceResponseBase {
+    data: Array<T> = [];
 }
